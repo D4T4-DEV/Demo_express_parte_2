c7c17926bf22da280d0ce4e905f718ba4e31a301
D4T4 <149961434+D4T4-DEV@users.noreply.github.com>
2024-03-13 17:26:50 -0600

    Soucion de bug de cantidad
    Se comenta la linea que daba error al calcular el total

--- server.js
+++ server.js
@@ -125,7 +125,7 @@ app.post('/actualizar-cantidad/:id/:cantidad', (req, res) => {
         const cantidadNueva = item.cantidad + cantidad;
         if (cantidadNueva > 0 && cantidad <= producto.cantidad) {
             item.cantidad = cantidadNueva;
-            item.precio = item.cantidad * producto.precio;
+            // item.precio = item.cantidad * producto.precio;
             producto.cantidad -= cantidad;
             if (item.cantidad === 0) {
                 carrito = carrito.filter(item => item.id !== idProducto);
