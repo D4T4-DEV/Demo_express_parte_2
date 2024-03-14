9e7388adbe0a43394054b15b860908a10ad28ddd
D4T4 <149961434+D4T4-DEV@users.noreply.github.com>
2024-03-13 19:28:41 -0600

    Diseño

--- controllers/users.js
+++ controllers/users.js
@@ -4,7 +4,7 @@
 const usuarios = [
     { id: 1, nombreUsuario: "Manolito", email: 'manolito@example.com', contrasenia: 'tilinInsanoXD' },
     { id: 2, nombreUsuario: "Manuelita", email: 'madrecitaBonita@example.com', contrasenia: 'mamarre' },
-    { id: 3, nombreUsuario: "Root", email: 'root@example.com', contrasenia: 'root' },
+    { id: 3, nombreUsuario: "Root", email: 'root@example.com', contrasenia: 'rootTumbaLaCasa' },
     { id: 4, nombreUsuario: "Anita", email: 'anita@example.com', contrasenia: 'AnitaMaxWin' }
 ];
 
